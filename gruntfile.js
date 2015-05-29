2acbba87eea0115f2842b3b01b2263ff68f6d87f
xiekun1992 <840914927@qq.com>
2015-05-29 08:29:39 +0800

    change

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports=function(grunt){
+	grunt.initConfig({
+		karma:{
+			unit:{
+				configFile:'./test/karma.conf.js'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-karma');
+
+	grunt.registerTask('default',['karma']);
+}
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,11 +17,7 @@ module.exports = function(config) {
     files: [
     '../node_modules/jquery/dist/jquery.js',
     '../node_modules/angular/angular.js',
-    '../node_modules/angular-mocks/angular-mocks.js',
-    '../node_modules/angular-cookies/angular-cookies.js',
-    '../node_modules/angular-sanitize/angular-sanitize.js',
-    '../node_modules/angular-resource/angular-resource.js',
-    '../node_modules/angular-ui-router/release/angular-ui-router.js',
+    '../node_modules/angular-*/angular-*.js',
     '../node_modules/moment/moment.js',
     '../client/javascripts/md5.js',
     '../client/app/*.js',
